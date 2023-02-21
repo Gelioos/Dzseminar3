695a3f26a2a7c76a5e7fd3be08d7e486637d83e6
Danil <martykhin84@gmail.com>
2023-02-21 21:57:44 +0300

    DZ

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// Console.WriteLine("Введите xa");
+// double xa = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Введите ya");
+// double ya = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Введите za");
+// double za = Convert.ToDouble(Console.ReadLine());
+
+
+// Console.WriteLine("Введите xb");
+// double xb = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Введите yb");
+// double yb = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Введите zb");
+// double zb = Convert.ToDouble(Console.ReadLine());
+
+// double Sc(double xa, double ya, double za, double xb, double yb, double zb)
+// {
+//     return Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+// }
+// Console.WriteLine(Sc(xa,ya,za,xb,yb,zb));
+
+
+
+// проверяет, является ли число  палиндромом.
+
+// Console.WriteLine("Введите 5-ти значное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// void Cel(int num)
+// {
+//     int fives = num % 10; 
+//     int fourth = ((num % 100) - fives) / 10; 
+//     int first = num / 10000;
+//     int then = (num/1000) % 10;
+//     if (fives == first && fourth == then)
+//     {
+//         Console.WriteLine("Число является палиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число не является палиндромом");
+//     }
+
+// }
+// Cel(num);
+
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num2 = 1;
+
+// void Qwert(int num)
+// {
+//     while(num2 < num)
+//     {
+//       Console.WriteLine(Math.Pow(num2, 3));
+//       num2++;
+//     }
+    
+
+// }
+// Qwert(num);
+
+
+
+
